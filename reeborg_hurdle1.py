ca1ead516903e2f3b5352f8f104e011c29f281a4
asmatamkeen <tamkeenasma06@gmail.com>
2026-02-21 23:16:39 +0530

    functions exercise-1

---
+++ reeborg_hurdle1.py
@@ -0,0 +1,16 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+def turn_around():
+    turn_left()
+    turn_left()
+for i in range(0,6):
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
